269fad5f3717565436020bfdf853d79efadb6780
Neeraja Djearaman <neeru.jai@gmail.com>
2020-06-05 12:44:56 +0100

    screenshot added

--- dissdoc/src/store/reducers/authReducer.js
+++ dissdoc/src/store/reducers/authReducer.js
@@ -17,7 +17,18 @@ const authReducer = (state = initstate, action) => {
         case 'SIGNOUT_SUCCESS':
             console.log('signed out successfully')
             return state;
-
+        case 'SIGNUP_SUCCESS':
+            console.log('sign up success');
+            return {
+                ...state,
+                authError: null
+            }
+        case 'SIGNUP_ERROR':
+            console.log('sign up error')
+            return {
+                ...state,
+                authError: action.err.message
+            }
 
         default:
             return state
